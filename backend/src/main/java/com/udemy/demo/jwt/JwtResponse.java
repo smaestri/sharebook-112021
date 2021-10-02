f9f3b2b44753235150d14aabdcfdd6d48935c42c
Sylvain Maestri <sylvainmaestri@gmail.com>
2021-10-02 13:09:43 +0200

    Section 5 - Security

---
+++ backend/src/main/java/com/udemy/demo/jwt/JwtResponse.java
@@ -0,0 +1,26 @@
+package com.udemy.demo.jwt;
+
+public class JwtResponse {
+    int userId;
+    String userName;
+    public JwtResponse(int id, String username) {
+        this.userId = id;
+        this.userName = username;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
