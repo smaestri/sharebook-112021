dbe69baaff7a9a924f151866d101257c3445f51f
Sylvain Maestri <sylvainmaestri@gmail.com>
2021-11-26 21:49:48 +0100

    Fix redirection URL
    Fix warning
    
    Fix conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ListBooks from './ListBooks'
 import MyBooks from './MyBooks'
 import AddBook from './AddBook'
@@ -6,9 +6,37 @@ import AddUser from './AddUser'
 import MyBorrows from './MyBorrows'
 import Header from './Header'
 import Login from './Login'
-import {BrowserRouter, Redirect, Route} from 'react-router-dom'
+import { BrowserRouter, Route, useHistory } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
+
+const UserConnected = ({ setUserInfo, userInfo }) => {
+  const history = useHistory();
+  useEffect(() => {
+    axios.get('/isConnected').then(response => {
+      if (history.location.pathname === "/") {
+        history.push("/listBooks")
+      }
+      else {
+        history.push(history.location.pathname)
+      }
+
+      setUserInfo(response.data)
+    }, () => {
+      console.error('user unknown')
+      history.push("/login")
+      setUserInfo(null)
+
+    })
+  }, [history, setUserInfo]);
+
+  return (<>
+    {userInfo && <Header userInfo={userInfo} setUserInfo={setUserInfo} />}
+  </>
+  )
+}
+
 
 function App() {
 
@@ -16,32 +44,31 @@ function App() {
 
   return (
     <div>
-      
+
       <BrowserRouter>
         <div className="container">
-            {!userInfo && <Redirect to="/login" /> }
-            {userInfo && <Header userInfo={userInfo} setUserInfo={setUserInfo} /> }
-            <Route path="/listBooks">
-              <ListBooks />
-            </Route>
-            <Route path="/myBooks">
-              <MyBooks />
-            </Route>
-            <Route exact path="/addBook">
-              <AddBook />
-            </Route>
-            <Route exact path="/addBook/:bookId">
-              <AddBook />
-            </Route>
-            <Route path="/myBorrows">
-              <MyBorrows />
-            </Route>
-            <Route path="/login">
-              <Login setUserInfo={setUserInfo} />
-            </Route>
-            <Route path="/addUser">
-              <AddUser setUserInfo={setUserInfo} />
-            </Route>
+          <UserConnected userInfo={userInfo} setUserInfo={setUserInfo} />
+          <Route path="/listBooks">
+            <ListBooks />
+          </Route>
+          <Route path="/myBooks">
+            <MyBooks />
+          </Route>
+          <Route exact path="/addBook">
+            <AddBook />
+          </Route>
+          <Route exact path="/addBook/:bookId">
+            <AddBook />
+          </Route>
+          <Route path="/myBorrows">
+            <MyBorrows />
+          </Route>
+          <Route path="/login">
+            <Login setUserInfo={setUserInfo} />
+          </Route>
+          <Route path="/addUser">
+            <AddUser setUserInfo={setUserInfo} />
+          </Route>
 
         </div>
       </BrowserRouter>
